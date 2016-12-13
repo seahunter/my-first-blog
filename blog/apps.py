d8a3bce16fa4a82d795df78e265c1142ca8a6c41
Seahunter <seahunter00@gmail.com>
2016-12-13 09:44:02 +0200

    Added views to create/edit blog post inside the site

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,8 @@
-from django.apps import AppConfig
+from django.conf.urls import url
+from . import views
 
-
-class BlogConfig(AppConfig):
-    name = 'blog'
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+]
